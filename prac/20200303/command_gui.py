c6b1eb51eebc4413426b3e8ca58f90f56a06571c
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-03-08 18:26:10 +0300

    Add program execution in command_gui

--- prac/20200303/command_gui.py
+++ prac/20200303/command_gui.py
@@ -1,4 +1,39 @@
 from tkinter import *
+import posix
+
+def execute_command():
+    pipe_fd = posix.pipe()
+    pid = posix.fork()
+    if pid == 0:
+        cmdline = ["lsblk"]
+        if check_fs_val.get() or check_UUID_val.get():
+            cmdline.append("-o")
+            col = "+"
+            if check_fs_val.get():
+                col += "fstype"
+            if check_fs_val.get() and check_UUID_val.get():
+                col += ","
+            if check_UUID_val.get():
+                col += "UUID"
+            cmdline.append(col)
+        posix.dup2(pipe_fd[1], 1)
+        posix.close(pipe_fd[0])
+        posix.close(pipe_fd[1]);
+        posix.execv("/bin/lsblk", cmdline)
+        quit()
+    else:
+        posix.close(pipe_fd[1])
+        ret = bytearray()
+        readbytes = posix.read(pipe_fd[0], 1000)
+        while readbytes != b"":
+            ret += readbytes
+            readbytes = posix.read(pipe_fd[0], 1000)
+        posix.close(pipe_fd[0])
+        posix.wait()
+        return str(ret, "utf-8")
+
+def button_press():
+    out_label["text"] = execute_command()
 
 main_frame = Frame()
 
@@ -13,19 +48,22 @@ main_frame.rowconfigure(0, weight = 0)
 options_frame = Frame(master = main_frame)
 options_frame.grid(sticky = "NEWS", column = 0, row = 0)
 
-butt_res = Button(master = options_frame, text = "lsblk")
+butt_res = Button(master = options_frame, text = "lsblk", command = button_press)
 butt_res.grid(sticky = "EW", column = 0, row = 0)
 
-check_fs = Checkbutton(master = options_frame, text = "Show file system type")
+check_fs_val = IntVar()
+check_fs = Checkbutton(master = options_frame, text = "Show file system type", variable = check_fs_val, onvalue = 1, offvalue = 0)
 check_fs.grid(sticky = "EWN", column = 0, row = 1)
 
-check_UUID = Checkbutton(master = options_frame, text = "Show file system UUID")
+check_UUID_val = IntVar()
+check_UUID = Checkbutton(master = options_frame, text = "Show file system UUID", variable = check_UUID_val, onvalue = 1, offvalue = 0)
 check_UUID.grid(sticky = "EWN", column = 0, row = 2)
 
-out_label = Label(master = main_frame, width = 80)
+out_label = Label(master = main_frame, width = 80, height = 40, justify = LEFT)
 out_label.grid(sticky = "NEWS", column = 1, row = 0);
 
 check_fs["cursor"] = "hand1"
 check_UUID["cursor"] = "hand1"
+
 
 mainloop()
