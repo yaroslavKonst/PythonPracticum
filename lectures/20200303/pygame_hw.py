dc20ffbc053544da444bc6ccf62ffcd63f247938
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-03-09 21:46:15 +0300

    Add timer usage to pygame_hw

--- lectures/20200303/pygame_hw.py
+++ lectures/20200303/pygame_hw.py
@@ -6,30 +6,30 @@ screen = pygame.display.set_mode((800, 600))
 SZ = 100, 80
 
 windows, nwin = [], 0
+
+timeout = 1000
+pygame.time.set_timer(pygame.USEREVENT, timeout)
+
 while True:
-    evs = pygame.event.get()
-    for e in evs:
-        if e.type is pygame.QUIT:
-            print("QUIT")
-            break
-        if e.type is pygame.MOUSEBUTTONDOWN:
-            if e.button == 3:
-                color = pygame.Color(randrange(100,256), randrange(100,256),randrange(100,256))
-                windows.append((nwin, color, pygame.Rect(e.pos, SZ)))
-                nwin += 1
-        else:
-            for (i, color, rect) in reversed(windows):
-                if hasattr(e, "pos") and rect.collidepoint(e.pos):
-                    print(f"{e} to {i}")
-                    break
-            else:
-                print(e)
-
+    e = pygame.event.wait()
+    if e.type is pygame.QUIT:
+        print("QUIT")
+        break
+    if e.type is pygame.MOUSEBUTTONDOWN:
+        if e.button == 3:
+            color = pygame.Color(randrange(100,256), randrange(100,256),randrange(100,256))
+            windows.append((nwin, color, pygame.Rect(e.pos, SZ)))
+            nwin += 1
     else:
-        screen.fill(0)
-        for i, color, rect in windows:
-            screen.fill(color, rect)
+        for (i, color, rect) in reversed(windows):
+            if hasattr(e, "pos") and rect.collidepoint(e.pos):
+                print(f"{e} to {i}")
+                break
+        else:
+            print(e)
 
-        pygame.display.flip()
-        continue
-    break
+    screen.fill(0)
+    for i, color, rect in windows:
+        screen.fill(color, rect)
+
+    pygame.display.flip()
