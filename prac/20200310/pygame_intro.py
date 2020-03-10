4211bfe316aa53ee884d08be14fbd8d3aa3dfa45
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-03-10 09:30:39 +0300

    Add new coordinate system to pygame_intro

---
+++ prac/20200310/pygame_intro.py
@@ -0,0 +1,31 @@
+import sys, pygame
+pygame.init()
+
+world_size = width, height = 10000, 9000
+speed = [100, 100]
+DX, DY = 0.1, 0.1
+pos = [width / 2, height / 2]
+screen_size = int(DX * width), int(DY * height)
+
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(screen_size)
+
+ball = pygame.image.load("intro_ball.gif")
+ballrect = ball.get_rect()
+w, h = ballrect.width / DX, ballrect.height / DY
+print(w, h)
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    pos[0] += DX * speed[0]
+    pos[1] += DY * speed[1]
+    if pos[0] - w/2 < 0 or pos[0] + w/2 > width:
+        speed[0] = -speed[0]
+    if pos[1] - h/2 < 0 or pos[1] + h/2 > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, (DX * (pos[0] - w/2), DY * (pos[1] - h/2)))
+    pygame.display.flip()
