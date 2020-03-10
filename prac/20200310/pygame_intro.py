6f3ed90b5cc65a1ae3f1c0669fc56cd6d6189c85
yaroslavKonst <yaroslav.konst@yandex.ru>
2020-03-10 09:36:41 +0300

    Remove coordinate multiplier in pygame_intro

--- prac/20200310/pygame_intro.py
+++ prac/20200310/pygame_intro.py
@@ -2,7 +2,7 @@ import sys, pygame
 pygame.init()
 
 world_size = width, height = 10000, 9000
-speed = [100, 100]
+speed = [2, 2]
 DX, DY = 0.1, 0.1
 pos = [width / 2, height / 2]
 screen_size = int(DX * width), int(DY * height)
@@ -19,13 +19,13 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    pos[0] += DX * speed[0]
-    pos[1] += DY * speed[1]
+    pos[0] += speed[0]
+    pos[1] += speed[1]
     if pos[0] - w/2 < 0 or pos[0] + w/2 > width:
         speed[0] = -speed[0]
     if pos[1] - h/2 < 0 or pos[1] + h/2 > height:
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, (DX * (pos[0] - w/2), DY * (pos[1] - h/2)))
+    screen.blit(ball, (int(DX * (pos[0] - w/2)), int(DY * (pos[1] - h/2))))
     pygame.display.flip()
